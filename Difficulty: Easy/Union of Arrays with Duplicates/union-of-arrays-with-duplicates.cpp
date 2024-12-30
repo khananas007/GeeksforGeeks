b3471c9383e2776bdf3f6c36168b255afe965ce0
Anas Khan <md.ak002266@gmail.com>
2024-12-30 20:58:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template in C++
+
+class Solution {
+  public:
+    // Function to return the count of number of elements in union of two arrays.
+     int findUnion(vector<int>& a, vector<int>& b) {
+        int count=0;
+        unordered_map<int,int> u;
+        for(int i:a)
+        {
+            if(!u[i])
+            {
+            count++;
+            u[i]++;
+            }
+        }
+        for(int i:b)
+        {
+            if(!u[i])
+            {
+            count++;
+            u[i]++;
+            }
+        }
+       return count;
+}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after reading t
+
+    while (t--) {
+        vector<int> a;
+        vector<int> b;
+
+        string input;
+        // For a
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // For b
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findUnion(a, b) << endl;
+        cout << '~' << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
